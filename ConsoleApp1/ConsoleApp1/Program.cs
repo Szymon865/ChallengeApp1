9e4e66aafc53e70306e5fed1a519cce38181bd49
GIGABYTE <spec865@gmail.com>
2023-02-15 14:26:08 +0100

    Zadanie 4

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,11 @@
+﻿string myName = "Szymon";
+string mySex = "Meszczyzna";
+var myAge = 36;
+var myAge2 = 30;
+string result = myName + mySex + myAge;
+
+if(myName != mySex)
+{
+    if (myAge2 < myAge) ;
+    Console.WriteLine("Kobieta poniżej 30 lat");
+}
